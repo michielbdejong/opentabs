6dbbb2d970c87a3a015137b20cd8ade7e43c7c0c
Michiel de Jong <michiel@unhosted.org>
2012-09-05 15:46:22 +0200

    got tab switching working - participants still jumbled

--- money.js
+++ money.js
@@ -5,7 +5,7 @@ remoteStorage.defineModule('money', function(myPrivateBaseClient, myPublicBaseCl
     },
     exports: {
       setDayBusiness: function(tab, year, month, day, transactions, endBalances) {
-        var datePath = year+'/'+month+'/'+day+'/'+tab.substring(1)+'/';
+        var datePath = tab+'/'+year+'/'+month+'/'+day+'/';
         for(var i=0; i<transactions.length;i++) {
           myPrivateBaseClient.storeObject('transaction', datePath+'transaction/'+i, transactions[i]);
         }
@@ -14,21 +14,42 @@ remoteStorage.defineModule('money', function(myPrivateBaseClient, myPublicBaseCl
         }
       },
       getTransactions: function(tab) {
-        if(tab=='#unhosted') {
-          return [{
-            from: 'michiel',
-            to: 'pot',
-            comment: 'received 4042 euros from paypal to his personal bank account (comdirect)',
-            amount: 4042,
-            currency: 'EUR',
-            date: '20111031'
-          }];
-        } else {
-          return [];
+        var transactions = [];
+        var years = myPrivateBaseClient.getListing(tab+'/');
+        for(var i=0;i<years.length;i++) {
+          var months = myPrivateBaseClient.getListing(tab+'/'+years[i]);
+          for(var j=0;j<months.length;j++) {
+            var days = myPrivateBaseClient.getListing(tab+'/'+years[i]+months[j]);
+            for(var k=0;k<days.length;k++) {
+              var dayTransactions = myPrivateBaseClient.getListing(tab+'/'+years[i]+months[j]+days[k]+'transaction/');
+              for(var l=0;l<dayTransactions.length;l++) {
+                transactions.push(myPrivateBaseClient.getObject(tab+'/'+years[i]+months[j]+days[k]+'transaction/'+dayTransactions[l]));
+              }
+            }
+          }
         }
+        return transactions;
       },
       getParticipants: function(tab) {
-        return ['michiel', 'javi', 'jan', 'hugo', 'martin', 'e.V.', 'nlnet', 'unhosted-reserved', 'berlinerbank', 'other', 'pot'];
+        var participantsMap = {};
+        var years = myPrivateBaseClient.getListing(tab+'/');
+        for(var i=0;i<years.length;i++) {
+          var months = myPrivateBaseClient.getListing(tab+'/'+years[i]);
+          for(var j=0;j<months.length;j++) {
+            var days = myPrivateBaseClient.getListing(tab+'/'+years[i]+months[j]);
+            for(var k=0;k<days.length;k++) {
+              var dayBalances = myPrivateBaseClient.getListing(tab+'/'+years[i]+months[j]+days[k]+'balance/');
+              for(var l=0;l<dayBalances.length;l++) {
+                participantsMap[dayBalances[l]]=true;
+              }
+            }
+          }
+        }
+        var participants = [];
+        for(var i in participantsMap) {
+          participants.push(i);
+        }
+        return participants;
       }
     }
   };
